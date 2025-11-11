443ab839bc35dc7bbe277672e5c8a2443dfe9915
nora-2222 <141223686+nora-2222@users.noreply.github.com>
2025-11-11 20:33:49 +0900

    Update comments for installation steps in batch file

--- install_store_and_more_app.bat
+++ install_store_and_more_app.bat
@@ -9,9 +9,7 @@ net session >nul 2>&1 || (
 echo off
 chcp 65001 >nul
 
-:: =====================================================
-:: 1. Install Microsoft Store
-:: =====================================================
+:: ===== 1. Install Microsoft Store =====
 echo.
 echo [1/8] Installing Microsoft Store...
 powershell -NoProfile -Command ^
@@ -34,44 +32,32 @@ try { ^
     echo 'Manual install: https://aka.ms/getstore' ^
 }"
 
-:: =====================================================
-:: 2. Install App Installer
-:: =====================================================
+:: ===== 2. Install App Installer =====
 echo.
 echo [2/8] Installing App Installer...
 winget install --id Microsoft.DesktopAppInstaller --source winget --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: 3. Install Windows Terminal
-:: =====================================================
+:: ===== 3. Install Windows Terminal =====
 echo.
 echo [3/8] Installing Windows Terminal...
 winget install --id 9N0DX20HK701 --source msstore --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: 4. Install PowerShell 7.x
-:: =====================================================
+:: ===== 4. Install PowerShell 7.x =====
 echo.
 echo [4/8] Installing PowerShell 7.x...
 winget install --id Microsoft.PowerShell --source winget --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: 5. Install Xbox App
-:: =====================================================
+:: ===== 5. Install Xbox App =====
 echo.
 echo [5/8] Installing Xbox App...
 winget install --id 9MV0B5HZVK9Z --source msstore --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: 6. Install Windows Notepad
-:: =====================================================
+:: ===== 6. Install Windows Notepad =====
 echo.
 echo [6/8] Installing Windows Notepad...
 winget install --id 9MSMLRH6LZF3 --source msstore --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: 7. Setup PowerShell & CMD Profiles
-:: =====================================================
+:: ===== 7. Setup PowerShell & CMD Profiles =====
 echo.
 echo [7/8] Setting up PowerShell & CMD profiles...
 set "PS7_PROFILE=%SystemDrive%\Users\Default\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
@@ -85,18 +71,14 @@ mkdir "%USERPROFILE%\Documents\PowerShell" 2>nul
 copy "%PS7_PROFILE%" "%USER_PS%" >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "cls" /f >nul
 
-:: =====================================================
-:: 8. Update Snipping Tool
-:: =====================================================
+:: ===== 8. Update Snipping Tool =====
 echo.
 echo [8/8] Updating Snipping Tool...
 powershell -Command "Get-AppxPackage *Snipping* -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue"
 powershell -Command "Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like '*Snipping*'} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue"
 winget install --id 9MZ95KL8MR0L --source msstore --accept-package-agreements --accept-source-agreements --silent --force
 
-:: =====================================================
-:: Complete
-:: =====================================================
+:: ===== Complete =====
 echo.
 echo [COMPLETE] All setup finished!
 exit /b 0
